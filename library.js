aa938714f6e71306db5a58e64c6e285b7cf3dd6d
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-05-26 20:12:48 +0800

    Layout the table structure

--- library.js
+++ library.js
@@ -1,4 +1,6 @@
-function Book(title, author, pages, read){
+const myLibrary = [];
+
+function Book(title, author, pages, read,id){
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
       }
@@ -6,7 +8,17 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = id;
     this.info = function(){
         return `${this.title} by ${this.author}, ${this.pages}, pages, ${this.read} `
     }
+}
+
+function addBookToLibrary(){
+    let id = crypto.randomUUID();
+
+}
+
+function displayOnPage(){
+    // display on page
 }
