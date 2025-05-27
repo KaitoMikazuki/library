004621da60f34e728be8832c2b9f42a14972f1d0
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-05-27 21:59:34 +0800

    Allow addBookToLibrary to push to myLibrary array. Simplify code logic by reducing unnecessary steps anf cuntions.

--- library.js
+++ library.js
@@ -1,13 +1,15 @@
 const myLibrary = [];
 
 function main(){
-    // Examples
-    const harry_potter = new Book("Harry Potter and the Sorcerer's Stone",'J.K. Rowling','223',false, crypto.randomUUID);
-    const atomic_habits = new Book("Atomic Habits", 'James Clear', '320', true, crypto.randomUUID);
-    myLibrary.push(harry_potter, atomic_habits);
+    // For demo purposes
+    const example1 = new Book("Harry Potter and the Sorcerer's Stone",'J.K. Rowling','223',false, crypto.randomUUID);
+    const example2 = new Book("Atomic Habits", 'James Clear', '320', true, crypto.randomUUID);
+    myLibrary.push(example1, example2);
 
+    // Add book buttons
     bookFormWindow = document.getElementById("bookFormWindow");
-    document.getElementById("addBook").addEventListener('click', () => openBookForm(bookFormWindow))
+    document.getElementById("addBook").addEventListener('click', () => bookFormWindow.showModal())
+    document.getElementById("close").addEventListener('click', () => bookFormWindow.close())
     document.getElementById("bookForm").addEventListener('submit', addBookToLibrary)
 }
 
@@ -16,7 +18,7 @@ function Book(title, author, pages, read, id){
         throw Error("You must use the 'new' operator to call the constructor");
       }
     this.title = title;
-    this.author = author;
+    this.author = author || "Unkown";
     this.pages = pages;
     this.read = read;
     this.id = id;
@@ -25,26 +27,22 @@ function Book(title, author, pages, read, id){
     }
 }
 
-function openBookForm(bookFormWindow){
-    bookFormWindow.showModal();
-}
-
 function addBookToLibrary(event){
     event.preventDefault();
     const bookData = new FormData(event.target);
 
     // Creates New book object with complete parameters
     const book = new Book(bookData.get('title'), bookData.get('author'), bookData.get('pages'), (bookData.get('status') !== null), crypto.randomUUID())
-    console.log(book)
+    console.log(book.info());
+    myLibrary.push(book);
+
+    // Resets the form and adds interactivity
+    event.target.reset();
+    alert(`Added "${book.title}" to the database!`);
 }
 
 function displayOnPage(){
     // display on page
-}
-
-
-function convertToLibraryItem(title){
-
 }
 
 main();
