0287c503857f7d6f5da78aff191fab7d6b384d53
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-05-28 10:43:37 +0800

    Add dynamic book item rendering with displayOnPage

--- library.js
+++ library.js
@@ -5,6 +5,7 @@ function main(){
     const example1 = new Book("Harry Potter and the Sorcerer's Stone",'J.K. Rowling','223',false, crypto.randomUUID);
     const example2 = new Book("Atomic Habits", 'James Clear', '320', true, crypto.randomUUID);
     myLibrary.push(example1, example2);
+    displayOnPage();
 
     // Add book buttons
     bookFormWindow = document.getElementById("bookFormWindow");
@@ -55,9 +56,23 @@ function addBookToLibrary(event){
     // Resets the form and adds interactivity
     event.target.reset();
     alert(`Added "${book.title}" to the database!`);
+    displayOnPage();
 }
 
 function displayOnPage(){
-    myLibrary.forEach()
+    const bookList = document.querySelector(".bookList");
+    bookList.innerHTML = ""
+    myLibrary.forEach((book) => {
+        const status = book.read ? "Read" : "Not read";
+        bookList.insertAdjacentHTML('beforeend', `
+            <tr class="book" id="${book.id}">
+                <td class="title">${book.title}</td>
+                <td class="author">${book.author}</td>
+                <td class="pages">${book.pages}</td>
+                <td><button class="status">${status}</button></td>
+                <td><button class="remove">x</button></td>
+            </tr>
+        `)
+    })
 }
 main();
